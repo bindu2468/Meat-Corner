f40f24d468b22d19fb30a11b8512470fd7a6ec95
bindu <gbindupriya123@gmail.com>
2020-09-25 18:18:01 -0700

    connecting to firebase

--- app/src/main/java/com/example/meatcircle/RegisterActivity.java
+++ app/src/main/java/com/example/meatcircle/RegisterActivity.java
@@ -3,12 +3,28 @@ package com.example.meatcircle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    //variables
+    TextInputLayout regName, regUsername, regEmail, regPhoneNo, regPassword;
+    Button regBtn, regToLoginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        //Hooks to all  elements in activity_register_up.xml
+        regName = findViewById(R.id.reg_name);
+        regUsername = findViewById(R.id.reg_username);
+        regEmail = findViewById(R.id.reg_email);
+        regPhoneNo = findViewById(R.id.reg_phoneNo);
+        regPassword = findViewById(R.id.reg_password);
+        regBtn = findViewById(R.id.reg_btn);
+        regToLoginBtn = findViewById(R.id.reg_login_btn);
     }
 }
